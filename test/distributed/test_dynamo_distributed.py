c46df85e62d78fa092caae8fda36ed967d4d092e
Syed Tousif Ahmed <syeahmed@nvidia.com>
2023-03-31 12:30:14 -0700

    Revert

--- test/distributed/test_dynamo_distributed.py
+++ test/distributed/test_dynamo_distributed.py
@@ -179,9 +179,11 @@ class TestFakeDistributedSingleProc(torch._dynamo.test_case.TestCase):
     @patch.object(config, "optimize_ddp", True)
     @patch.object(torch._inductor.config, "fallback_random", True)
     def test_hf_bert_ddp_inductor(self):
-        model, inputs = get_hf_bert(0)
-        model = FakeDDP(model)
-        run_hf_bert_ddp(self, model, inputs, "inductor")
+
+        with _dynamo_dist_per_rank_init(self.rank, self.world_size):
+            model, inputs = get_hf_bert(self.rank)
+            model = DDP(model)
+            run_hf_bert_ddp(self, model, inputs, "inductor")
 
     @patch.object(config, "optimize_ddp", True)
     def test_hf_bert_ddp_aot_eager(self):
